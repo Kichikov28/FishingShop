e0c125874eaffde9c8655fe469cfe7bf9cab8787
Kichikov28 <dkichikov4@gmail.com>
2023-08-21 21:51:31 +0300

    FormApp start

---
+++ FishingShop/FishingShop.FormApp/MainForm.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace FishingShop.FormApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnShop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnShop
+            // 
+            this.btnShop.Font = new System.Drawing.Font("Showcard Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnShop.Location = new System.Drawing.Point(66, 281);
+            this.btnShop.Name = "btnShop";
+            this.btnShop.Size = new System.Drawing.Size(152, 58);
+            this.btnShop.TabIndex = 0;
+            this.btnShop.Text = "Shops";
+            this.btnShop.UseVisualStyleBackColor = true;
+            this.btnShop.Click += new System.EventHandler(this.btnShop_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Menu;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnShop);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnShop;
+    }
+}
---
+++ FishingShop/FishingShop.FormApp/MainForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FishingShop.FormApp
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnShop_Click(object sender, EventArgs e)
+        {
+            ShopsForm shopsForm= new ShopsForm();
+            shopsForm.ShowDialog();
+        }
+    }
+}
